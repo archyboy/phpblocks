0354692947695b758065c3593e6bb3e91522e63a
archyboy <archy@workmail.com>
2024-12-01 07:25:07 +0100

    Update block_start.php

--- application/cv/block/block_start.php
+++ application/cv/block/block_start.php
@@ -5,7 +5,7 @@ class Block_Start extends \System\Block {
 
     public function __construct() {
         $this->view = new \System\View(parent::$config->TEMPLATE_ACTIVE_DIRECTORY . 't_start.php');
-        $this->view->add('TITLE','CV - Anders Habberstad');
+        $this->view->add('TITLE','CV - MyName');
         $this->view->execute();
 
     }
